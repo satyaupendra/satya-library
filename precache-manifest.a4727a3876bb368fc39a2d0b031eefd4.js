cdaa025954b294d4003f88b5df099a4adfd2cd00
satya <satyaupendra.devabhaktineni9@gmail.com>
2020-06-07 15:43:51 +0530

    Updates

---
+++ precache-manifest.a4727a3876bb368fc39a2d0b031eefd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78da73dc1ff2e594725d5108ccf23f67",
+    "url": "/satya-library/index.html"
+  },
+  {
+    "revision": "404f858a1bfd6b1c0a62",
+    "url": "/satya-library/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "ed4d1c3c308022a93f1b",
+    "url": "/satya-library/static/css/main.e2248179.chunk.css"
+  },
+  {
+    "revision": "404f858a1bfd6b1c0a62",
+    "url": "/satya-library/static/js/2.35d17c62.chunk.js"
+  },
+  {
+    "revision": "ed4d1c3c308022a93f1b",
+    "url": "/satya-library/static/js/main.bb4e836a.chunk.js"
+  },
+  {
+    "revision": "a0602d8f00e99e4171e9",
+    "url": "/satya-library/static/js/runtime-main.500a049c.js"
+  },
+  {
+    "revision": "76f3d7204a22276ad8013deecea70c7c",
+    "url": "/satya-library/static/media/logo.76f3d720.svg"
+  }
+]);
